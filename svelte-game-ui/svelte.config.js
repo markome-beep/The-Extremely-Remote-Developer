034dd6214ddc5607adfd74a2fa9d5bd76300a9ee
markome-beep <guyofsoccer@gmail.com>
2025-04-07 23:47:15 -0400

    Fixed Deploy to include building wasm bin

--- svelte-game-ui/svelte.config.js
+++ svelte-game-ui/svelte.config.js
@@ -1,9 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const basePath = process.env.BASE_PATH || ''; // important for github pages
+
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	paths: {
+		base: basePath
+	}
 };
 
 export default config;
